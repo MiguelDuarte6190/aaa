68eba947554b8ce662d07814cd072a4aedd49815
MiguelDuarte6190 <143223125+MiguelDuarte6190@users.noreply.github.com>
2023-10-21 11:13:48 -0300

    Add files via upload

---
+++ hola-mundo.js
@@ -0,0 +1,10 @@
+alert("Bienvenido");
+var nombre = prompt("Escribe tu nombre","");
+alert("Que onda " + nombre);
+
+var x = 7;
+var y = 3;
+
+var suma = x + y;
+
+alert ("La suma de x e y es: " + suma)
